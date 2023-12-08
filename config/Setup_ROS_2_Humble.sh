c005365f42df33e43c6da8899fe791892594494c
slowrunner <slowrunner@users.noreply.github.com>
2023-12-08 10:01:44 -0500

    per docs

--- config/Setup_ROS_2_Humble.sh
+++ config/Setup_ROS_2_Humble.sh
@@ -2,27 +2,37 @@
 
 # (Headless) SETUP ROS2 GoPiGo3 (HUMBLE HAWKSBILL) ON Ubuntu 22.04 64-bit Server (Jammy Jellyfish)
 
-# === Make sure system is up to date before continuing
-sudo apt update
-sudo apt upgrade -y
+
+# == Check for UTF-8 locale
+sudo apt update && sudo apt install -y locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+locale
+
+# == Ensure Ubuntu Universe repo is enabled
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
 
 # === Setup GPG key to ROS2
-sudo apt update && sudo apt install curl gnupg lsb-release
+sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
 
 # === Add ROS2 Repository to sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# === Install ROS2 Humble Hawksbill basic packages
-sudo apt install -y ros-humble-ros-base
+# === Make sure system is up to date before continuing
+sudo apt update
+sudo apt upgrade -y
+
+# === Install ROS2 Humble Hawksbill desktop packages
+sudo apt install -y ros-humble-ros-desktop
+
+# === Install ROS 2 Compilers and tools to build packages
+sudo apt install -y ros-dev-tools
 
 # === Source setup script
 source /opt/ros/humble/setup.bash
-
-# === Install Demo Nodes (Demos and Tutorials not installed by the ROS-base package)
-sudo apt install -y ros-humble-demo-nodes-py
-sudo apt install -y ros-humble-demo-nodes-cpp
 
 # === Setup colcon build tool
 sudo apt install -y python3-colcon-common-extensions
@@ -44,39 +54,28 @@ pip3 install transforms3d
 sudo apt install -y python3-rosdep2
 rosdep update
 
-# === INSTALL SAFE IMU CLASS
-sudo pip3 install imu4gopigo3ros2
-
 # ==== Create a ROS2 Workspace with source folder
 
-mkdir -p ~/ros2ws/src
+mkdir -p ~/wali_desk/c3ws/src
 
 === Setup colcon_cd in .bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 
 # === Setup colcon to know where workspace is
-echo "export _colcon_cd_root=~/ros2ws" >> ~/.bashrc
+echo "export _colcon_cd_root=~/wali_desk/c3ws" >> ~/.bashrc
 
-# setup to run GoPiGo3 ROS2 nodes when login 
-echo "source ~/ros2ws/install/setup.bash" >> ~/.bashrc
-
-# === Bring down ROS2 GoPiGo3 nodes and helper scripts
-wget https://github.com/slowrunner/ROS2-GoPiGo3/raw/main/gopigo3ros2ws.tgz
-tar -xzvf gopigo3ros2ws.tgz -C ~/ros2ws
+# setup to know about built packages 
+echo "source ~/wali_desk/c3ws/install/setup.bash" >> ~/.bashrc
 
 # === SETUP ROSDEP
 sudo apt install -y python3-rosdep2
 rosdep update
 
-# === INSTALL SAFE IMU CLASS 
-sudo pip3 install imu4gopigo3ros2
-
-
 
 # === Build everything
 . ~/.bashrc
-cd ~/ros2ws
-./build_all.sh
-. install/setup.bash
+cd ~/wali_desk/c3ws
+# ./build_all.sh
+# . install/setup.bash
 
-echo -e "\n********** ROS2 GOPIGO3 READY FOR TEST ***********\n"
+echo -e "\n********** ROS2 HUMBLE READY FOR TEST ***********\n"
