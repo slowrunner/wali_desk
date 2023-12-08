d4dd369d1357f0ad71223a4e6b110ab6b4843b1d
slowrunner <slowrunner@users.noreply.github.com>
2023-12-08 14:03:42 -0500

    Create Setup_Ignition.sh

---
+++ config/Setup_Ignition.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# REF: https://github.com/iRobotEducation/create3_sim#ignition-fortress
+
+sudo apt-get update && sudo apt-get install wget
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update && sudo apt-get install ignition-fortress
