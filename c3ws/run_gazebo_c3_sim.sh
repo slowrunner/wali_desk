058372ebeb9ae4b6657e92397cba243edf60666c
slowrunner <slowrunner@users.noreply.github.com>
2023-12-09 11:09:52 -0500

    working

--- c3ws/run_gazebo_c3_sim.sh
+++ c3ws/run_gazebo_c3_sim.sh
@@ -2,6 +2,6 @@
 
 echo "Starting Create3 Sim in Gazebo"
 cd ~/wali_desk/c3ws
-source install/local_setup.bash
+source install/setup.bash
 
-ros2 launch irobot_create_gazebo create3.launch.py
+ros2 launch irobot_create_gazebo_bringup create3_gazebo.launch.py
