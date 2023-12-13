bcb3b4a88f7276c7e4cf86c2695cc5f02af7c3cf
slowrunner <slowrunner@users.noreply.github.com>
2023-12-13 11:02:37 -0500

    handle if no install/setup.bash yet

--- c3ws/g_run_gazebo_c3_sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Starting Create3 Sim in Gazebo"
-cd ~/wali_desk/c3ws
-# source install/setup.bash
-
-ros2 launch irobot_create_gazebo_bringup create3_gazebo.launch.py
--- c3ws/run_gazebo_c3_sim.sh
+++ c3ws/run_gazebo_c3_sim.sh
@@ -2,6 +2,8 @@
 
 echo "Starting Create3 Sim in Gazebo"
 cd ~/wali_desk/c3ws
-source install/setup.bash
+if [ -e install/setup.bash ]; then
+  source install/setup.bash
+fi
 
 ros2 launch irobot_create_gazebo_bringup create3_gazebo.launch.py
