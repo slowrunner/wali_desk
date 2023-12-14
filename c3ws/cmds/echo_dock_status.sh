20fe2d7605c78efd9fa9ec94c973b70309ab0273
slowrunner <slowrunner@users.noreply.github.com>
2023-12-14 12:54:51 -0500

    wali and battery status nodes, life.log (but not logLife)

--- c3ws/cmds/echo_dock_status.sh
+++ c3ws/cmds/echo_dock_status.sh
@@ -3,8 +3,8 @@
 echo -e "\n*** ECHO DOCK STATUS"
 if [ $ROS_DISTRO == "galactic" ]
   then
-    echo -e "ros2 topic echo /dock_status"
-    ros2 topic echo /dock_status
+    echo -e "ros2 topic echo /dock"
+    ros2 topic echo /dock
   else
     echo -e "ros2 topic echo --once /dock_status"
     ros2 topic echo --once /dock_status
