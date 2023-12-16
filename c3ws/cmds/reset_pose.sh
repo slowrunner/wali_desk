91f67eb48244373ae9334ae473cdd8ee5b9027c7
slowrunner <slowrunner@users.noreply.github.com>
2023-12-16 11:59:23 -0500

    learning about coord frame (0 deg facing away from dock)

--- c3ws/cmds/reset_pose.sh
+++ c3ws/cmds/reset_pose.sh
@@ -4,12 +4,20 @@
 
 echo -e '\n** Fix Odom Drift with reset_pose 0,0/0'
 
-echo -e '** CURRENT ODOM'
-ros2 topic echo --once --flow-style /odom
+if [ $ROS_DISTRO != "galactic" ]; then  
+  echo -e '** CURRENT ODOM'
+  ros2 topic echo --once --flow-style /odom
+fi
 
 echo -e '** SEND RESET_POSE'
 ros2 service call /reset_pose irobot_create_msgs/srv/ResetPose "{}"
 
-echo -e '** New Odom'
-ros2 topic echo --once --flow-style /odom
+# To Reset To particular pose:
+# ros2 service call /reset_pose irobot_create_msgs/srv/ResetPose "pose: {position: {x: 1, y: 2, z: 0}, orientation: {x: 0, y: 0, z: 0, w: 1}}"
+
+if [ $ROS_DISTRO != "galactic" ]; then  
+  echo -e '** New Odom'
+  ros2 topic echo --once --flow-style /odom
+fi
+
 echo -e '********\n'
